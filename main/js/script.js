73aaf945d9f2115f59f33d6db6e5976ffcdd92ec
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2021-01-27 19:37:12 +0300

    d

--- main/js/script.js
+++ main/js/script.js
@@ -11,14 +11,29 @@ $(".center__slider").slick({
   pauseOnHover: true,
   autoplaySpeed: 3000,
   arrows: true,
-  autoplay: true,
+//   autoplay: true,
   prevArrow: ".slider__pre",
   nextArrow: ".slider__next",
   dots: true
 });
-plus.addEventListener('click', () => {
-    plus__media.style.transform = "rotate(90deg)";
-    popup.style.display = "flex";
+let deg1 = 0;
+let deg2 = 0;
+let deg3 = 0;
+
+plus1.addEventListener('click', () => {
+    deg1 += 90;
+    plus__media1.style.transform = `rotate(${deg1}deg)`;
+    setTimeout(() => {
+        popup.style.display = "flex";
+    }, 500)
+})
+plus2.addEventListener('click', () => {
+    deg2 += 90;
+    plus__media2.style.transform = `rotate(${deg2}deg)`;
+})
+plus3.addEventListener('click', () => {
+    deg3 += 90;
+    plus__media3.style.transform = `rotate(${deg3}deg)`;
 })
 btn.addEventListener('click', () => {
     popup.style.display = "none";
